8b22a05a364a2b424f19079d84bd5fe26da91668
Allison <allisonmeier01@gmail.com>
2023-04-11 13:46:42 -0400

    prints all scripts to txt file; wrong ep/szn nums

--- scraper.py
+++ scraper.py
@@ -1,14 +1,15 @@
 import urllib
+import pandas
 import pip._vendor.requests as requests
 from bs4 import BeautifulSoup
+from os import listdir
+from os.path import isfile, join
+from html.parser import HTMLParser
  
 # here we have to pass url and path
 # (where you want to save your text file)
 #urllib.request.urlretrieve("https://www.geeksforgeeks.org/grep-command-in-unixlinux/?ref=leftbar-rightbar",
  #                          "/home/gpt/PycharmProjects/pythonProject1/test/text_file.txt")
-
-
-allLinks = []
 
 site_url = 'https://transcripts.foreverdreaming.org/'
 forum_url = 'https://transcripts.foreverdreaming.org/viewforum.php?f=463'
@@ -17,6 +18,8 @@ forum_url = 'https://transcripts.foreverdreaming.org/viewforum.php?f=463'
 #https://transcripts.foreverdreaming.org/viewforum.php?f=463
 #https://tvshowtranscripts.ourboard.org/viewforum.php?f=463
 
+
+dataFile = 'dataFile.txt'
 
 # create all the files we need. i do not want to type all of this!!
 def createFiles():
@@ -28,44 +31,50 @@ def createFiles():
 
 # obtain the actual *stuff* to put into the files
 def getText(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    forum = requests.get(url)
+    soup = BeautifulSoup(forum.content, 'html.parser')
     divs = soup.find_all("a", {"class":"topictitle"})
     del divs[0] # snip off the unnecessary extra forum link
 
     specific_links = []
+
     for div in divs:
-        link = str(div['href'])[2:]
-        specific_links.append(link)
         text = str(div)
-    # episodeIndex = text.find('>')+1
-    # as of here, each episode page can be found at site_url + specific_links[i]
-
-
-# throw all the stuff in there
-def fillFiles():
-    i = 1
-
-    for element in allFiles:
-        file = open(element, "r") # 'r' = read mode 
-        content = file.read()
-
-        page = urllib.request.urlretrieve()
-
-        soup = BeautifulSoup(page.content, 'html.parser')
+        link = str(div['href'])[2:]
+        specific_links.append(site_url+link)
+        ti = text.find('>')+1
+        global episode
+        episode = text[ti+3:ti+5]
+        global season
+        season = text[ti:ti+2]
     
-        f = open(allLinks[i], "w")
+    file = open(dataFile, "w")
 
-        i+=1
+    for link in specific_links:
+        page = urllib.request.urlretrieve(link)
+        #print(page)
+        #newSoup = BeautifulSoup(link.content, 'html.parser')
+        
+        data = BeautifulSoup(requests.get(link).content, 'html.parser').find("div", {"class":"content"})
 
-        print(i)
-    
-    # traverse article guts from soup
-    for data in soup.find_all("article"):
+        #print(data)
+
+        #f = open(link, "r")
+
         sum = data.get_text()
-        f.writelines(sum)
+        file = open(dataFile, "a", encoding='utf-8')
+        file.write('\n\n************************************************************************************\nSEASON: '+season+'\nEPISODE: '+episode+'\n************************************************************************************\n\n')
+        file.writelines(sum)
     
-    f.close()
+    file.close()
+
+    #data = data.split("\n") # then split it into lines
+
+    #for line in data:
+        #print(line)
+
+    #print(soup.get_text('\n'))
+
 
 
 getText(forum_url)
