3acc59adc098b6f89e8e10e0ee8cbead4c8e8632
Allison <allisonmeier01@gmail.com>
2023-04-19 23:57:38 -0400

    got list of characters, speakers, freq, + sketch

--- scraper.py
+++ scraper.py
@@ -51,6 +51,56 @@ def getText(links):
 # DON'T DO THAT THOUGH, or else you'll have to un-censor everything all over again
 
 
+# Remove sfx like '[cheering]' '[waves crashing]' and returns list of each speaker instance like '[Character1]' '[Character2]
+def removeSFX(filename):  # 2146 without isUpper check; 1655 with it; 1008 after getting rid of randomSFX; 53 names total
+    roughBracketContents = []
+    randomSFX = ['screaming', 'exhales', 'panting', 'inaudible', 'coughing', 'kisses', 'chuckles', 'groans', 'all', 'exclaims', 
+                 'yelling', 'weakly', 'softly', 'gasps', 'crying', 'praying', 'thud', 'whimpers', 'whimpering', 'laughing', 
+                 'choking', 'thudding', 'grunting', 'grunts', 'scoffs', 'sighs', 'inhales', 'musician', 'laughs', 'man', 'screams', 
+                 'caws', 'clatters', 'soldier', 'snoring', 'sniffles', 'sobbing', 'clucking', 'coughs', 'retching', 'stuttering', 
+                 'sniffs', 'stutters', 'thuds', 'whistles', 'shushes', 'groaning', 'straining', 'nun', 'sobs', 'gasping', 
+                 'shuddering', 'urinating', 'sable', 'echoing', 'vomits', 'boy', 'shakily', 'monk', 'dane', 'guard', 'whispers', 
+                 'woman', 'driver', 'stammers', 'yells', 'snickers', 'snorts', 'mercian', 'stammering', 'cheering', 'laughter', 
+                 'clattering', 'footsteps', 'shouting', 'silence', 'thunk!', 'murmuring', 'chuckling', 'cheers', 'whinnying', 
+                 'snarling', 'moaning', 'sniffling', 'giggles', 'spitting', 'yawns', 'knocking', 'sigh', 'chuckle', 'scoff', 'clucks', 
+                 'gasp', 'clang', 'sniffle', 'thump', 'spits', 'wheezing', 'hisses', 'shivering', 'slap', 'whistling', 'moans', 'moan', 'cries']
+
+    file = open(filename, 'r')
+    for line in file:
+        if '[' in line:
+            bracketContents = line[line.index('[') + 1 : line.index(']')]
+            if (' ' not in bracketContents and bracketContents[0].isupper()):
+                bracketContents = bracketContents.lower()
+                roughBracketContents.append(bracketContents)
+
+    speakerInstances = []
+    [speakerInstances.append(x.capitalize()) for x in roughBracketContents if x not in randomSFX]
+
+    file.close()
+
+    return speakerInstances
+
+
+def getCharacterList(speakerInstances):
+    characterNames = []
+    [characterNames.append(x) for x in speakerInstances if x not in characterNames]
+
+    return characterNames # should be 53 
+
+
+def getSpeakingCount(speakerInstances):
+
+    nameCount = {}
+    for name in speakerInstances:
+        if name in nameCount:
+            nameCount[name] += 1
+        else: 
+            nameCount[name] = 1
+
+    for key, value in nameCount.items():
+        print(f"{key}: {value}")
+
+    return nameCount
 
 
 # TODO: Most likely need to get all my text into a csv somehow 
