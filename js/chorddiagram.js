b726e52a73fcfde86a90485227d939e399c36b51
Allison <allisonmeier01@gmail.com>
2023-04-26 01:41:45 -0400

    chord diagram is beautiful!!!

--- js/chorddiagram.js
+++ js/chorddiagram.js
@@ -8,15 +8,18 @@ class ChordDiagram {
             containerWidth: defaultConfig.containerWidth || 600,
             containerHeight: defaultConfig.containerHeight || 600,
             margin: {top: 5, right: 5, bottom: 20, left: 20},
+            tooltipPadding: defaultConfig.tooltipPadding || 15,
         }
         this.data = _data
-        this.mainCharacters = mainCharacters
+        this.mainCharacters = mainCharacters.slice(0,10)
 
         this.initVis()
 	}
 	
 	initVis() {
 		let vis = this
+
+        vis.colors = ['#F25041', '#F25041', '#38B000', '#F25041', '#F25041', '#8C1F66', '#33A6A6', '#323673', '#F25041', '#323673' ]
 
 		vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right
 	  	vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom
@@ -43,9 +46,10 @@ class ChordDiagram {
                 .attr('class', 'chord-titles')
                 .attr('x', '5')
                 .attr('dy', '.35em')
-                .attr('text-anchor', d => { d.angle > Math.PI ? 'end' : null}) // if the chord angle isnt possible, get it out of here
+                .attr('text-anchor', d => { (d.angle > Math.PI) ? 'end' : null}) // if the chord angle isnt possible, get it out of here
+                .attr('font-size', '11')
                 .attr('font-family', 'sans-serif')
-                .attr('transform', d => { `rotate(${(d.angle * 180)/Math.PI - 90})translate(${270})${d.angle > Math.PI? "rotate(180)" : ""}` })
+                .attr('transform', d => {return "rotate(" +(d.angle*180/Math.PI - 90) + ")" + "translate(" + (272) + ")" + (d.angle>Math.PI? "rotate(180)" : "")})
                 .style('fill', 'black')
                 .text(d => vis.mainCharacters[d.index])
 
@@ -59,8 +63,9 @@ class ChordDiagram {
                 .attr('class', d => {return 'speaker ' + vis.mainCharacters[d.index] })
                 .attr('id', d => { return '#speaker ' + vis.mainCharacters[d.index] })
                 .style('stroke', 'black')
-                .style('fill', 'blue') // fix later
-                .style('opacity', '0.6')
+                //.style()
+                .style('fill', d => vis.colors[d.index]) // fix later
+                .style('opacity', '0.7')
                 .attr('d', d3.arc().innerRadius(200).outerRadius(210))
             
         vis.svg // chord links between groups
@@ -71,9 +76,24 @@ class ChordDiagram {
             .data(d => d)
             .join('path')
                 .attr('d', d3.ribbon().radius(200))
-                .style('fill', 'purple') //update later
+                .style('fill', d => vis.colors[d.source.index%19]) //update later
+                .style('opacity', '0.5')
                 .style('stroke', 'black')
-            // add tooltips here later
+            .on('mouseover', (event, d) => {
+                d3.select('#tooltip')
+                    .style('display', 'block')
+                    .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')
+                    .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
+                    .html(`
+                        <div class="tooltip-title">${vis.mainCharacters[d.source.index]}:</div>
+                        <ul>
+                        <li>Mentioned ${vis.mainCharacters[d.target.index]} ${d.source.value} times</li>
+                        <li>Mentioned by ${vis.mainCharacters[d.target.index]} ${d.target.value} times</li>
+                        </ul>`)
+                })
+            .on('mouseleave', () => {
+                d3.select('#tooltip').style('display', 'none')
+            })
 
 
 
@@ -103,8 +123,6 @@ class ChordDiagram {
         vis.data.forEach(d => {
             //console.log('data: ', d.dialogue)
             let characterMention = new Set(d.dialogue.match(mainCharactersRE))
-            console.log(characterMention)
-
             if (characterMention.size != 0 && d.character.match(mainCharactersRE)) {
                 characterMention.forEach(cm => {
                     let speakerIndex = vis.mainCharacters.indexOf(d.character)
